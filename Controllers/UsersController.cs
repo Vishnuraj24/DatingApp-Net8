fb9de7a46cd7e3c14022bd3baf3db18bf04566dd
Kandukuri Vishnuraj <k.vishnurajchary@gmail.com>
2025-02-16 20:00:12 +0530

    My First Commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(DataContext dbContext) : ControllerBase
+{
+
+    [HttpGet] // /api/users
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await dbContext.Users.ToListAsync();
+
+        return Ok(users);
+    }
+
+    [HttpGet("{id}")] // /api/users/3
+    public ActionResult<AppUser> GetUser(int id)
+    {
+        var user = dbContext.Users.Find(id);
+
+        if (user == null) return NotFound();
+
+        return user;
+    }
+}
+
